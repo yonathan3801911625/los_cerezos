67523fa701b1d09bd7c741f9f2a87e5d21f4dd3a
NorbertCiro <salazarbritonorberto@gmail.com>
2022-09-27 22:24:44 -0500

    Se modifica vistas de index y create de los controladores

--- resources/views/insumos/formulario.blade.php
+++ resources/views/insumos/formulario.blade.php
@@ -1,43 +1,50 @@
 @csrf
 <div class="block">
-    <x-jet-label for="codigo" value="{{ __('Codigo') }}" />
-    <x-jet-input id="codigo" class="block mt-1 w-full" type="text" name="codigo" :value="old('codigo', $insumo->codigo)" required autofocus />
-
-    <x-jet-label for="nombre" value="{{ __('Nombre') }}" />
-    <x-jet-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre', $insumo->nombre)" required autofocus />
-
-    <div class="my-1">
-        <x-jet-label for="tipo">Selecciona la unidad de medida</x-jet-label>
-        <select name="unidad" id="unidad">
-            <option value="Kg">Kg</option>
-            <option value="M">M</option>
-            <option value="M2">M2</option>
-            <option value="L">L</option>
-        </select>
+    <div class="my-2 px-6">
+        <x-jet-label for="codigo" value="{{ __('Codigo') }}" />
+        <x-jet-input id="codigo" class="block mt-1 w-full" type="text" name="codigo" :value="old('codigo', $insumo->codigo)" required autofocus />
     </div>
-
-    <x-jet-label for="precio" value="{{ __('precio') }}" />
-    <x-jet-input id="precio" class="block mt-1 w-full" type="text" name="precio" :value="old('precio', $insumo->precio)" required autofocus />
-
-    <x-jet-label for="cantidad" value="{{ __('Cantidad') }}" />
-    <x-jet-input id="cantidad" class="block mt-1 w-full" type="number" name="cantidad" :value="old('cantidad', $insumo->cantidad)" required autofocus />
-
-    <x-jet-label for="tipo" value="{{ __('Tipo') }}" />
-    <x-jet-input id="tipo" class="block mt-1 w-full" type="text" name="tipo" :value="old('tipo', $insumo->tipo)" required autofocus />
-
-    <x-jet-label for="fecha_vencimiento" value="{{ __('Fecha_vencimiento') }}" />
-    <x-jet-input id="fecha_vencimiento" class="block mt-1 w-full" type="date" name="fecha_vencimiento" :value="old('fecha_vencimiento', $insumo->fecha_vencimiento)" required autofocus />
-
-
-    <div class="flex justify-center">
-        <div class="p-2">
-            <div class="flex justify-end">
-                <a href="{{ route('insumos.create') }}">
-                    <x-jet-button type="submit">
-                        Guardar
-                    </x-jet-button>
-            </div>
+    <div class="my-2 px-6">
+        <x-jet-label for="nombre" value="{{ __('Nombre') }}" />
+        <x-jet-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre', $insumo->nombre)" required autofocus />
+    </div>
+    <div class="my-1">
+        <div class="my-2 px-6">
+            <x-jet-label for="tipo">Selecciona la unidad de medida</x-jet-label>
+            <select name="unidad" id="unidad">
+                <option value="Kg">Kg</option>
+                <option value="M">M</option>
+                <option value="M2">M2</option>
+                <option value="L">L</option>
+            </select>
         </div>
     </div>
+    <div class="my-2 px-6">
+        <x-jet-label for="precio" value="{{ __('precio') }}" />
+        <x-jet-input id="precio" class="block mt-1 w-full" type="text" name="precio" :value="old('precio', $insumo->precio)" required autofocus />
+    </div>
+    <div class="my-2 px-6">
+        <x-jet-label for="cantidad" value="{{ __('Cantidad') }}" />
+        <x-jet-input id="cantidad" class="block mt-1 w-full" type="number" name="cantidad" :value="old('cantidad', $insumo->cantidad)" required autofocus />
+    </div>
+    <div class="my-2 px-6">
+        <x-jet-label for="tipo" value="{{ __('Tipo') }}" />
+        <x-jet-input id="tipo" class="block mt-1 w-full" type="text" name="tipo" :value="old('tipo', $insumo->tipo)" required autofocus />
+    </div>
+    <div class="my-2 px-6">
+        <x-jet-label for="fecha_vencimiento" value="{{ __('Fecha_vencimiento') }}" />
+        <x-jet-input id="fecha_vencimiento" class="block mt-1 w-full" type="date" name="fecha_vencimiento" :value="old('fecha_vencimiento', $insumo->fecha_vencimiento)" required autofocus />
 
-</div>
+
+        <div class="flex justify-center">
+            <div class="p-2">
+                <div class="flex justify-end">
+                    <a href="{{ route('insumos.create') }}">
+                        <x-jet-button type="submit">
+                            Guardar
+                        </x-jet-button>
+                </div>
+            </div>
+        </div>
+
+    </div>
