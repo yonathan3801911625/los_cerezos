c2ff2c763b0e13e4e345873d729bd5cbd10a7075
norbertsalazar <salazarbritonorberto@gmail.com>
2022-10-24 13:54:08 -0500

    Se modica botones de fase

--- resources/views/cultivos/edit.blade.php
+++ resources/views/cultivos/edit.blade.php
@@ -26,7 +26,7 @@
                         <x-jet-label for="fase">Selecciona una fase</x-jet-label>
                         <select name="fase" id="fase">
                             @foreach ($fases as $fase)
-                            <option value="{{ $fase->id }}">{{ $fase->nombre }}</option>
+                                <option value="{{ $fase->id }}">{{ $fase->nombre }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -35,54 +35,57 @@
             </div>
 
             @if (count($cultivo_fases))
-            <div class="my-6">
+                <div class="my-6">
 
-                <div class="mt-5 mb-3">
-                    <h3 class="h3">
-                        Fases
-                    </h3>
-                </div>
+                    <div class="mt-5 mb-3">
+                        <h3 class="h3">
+                            Fases
+                        </h3>
+                    </div>
 
-                @foreach ($cultivo_fases as $cultivo_fase)
-                <div class="card mb-4">
-                    <div class="card-header">
-                        <div class="justify-between d-flex align-items-center">
-                            <h4 class="h4 mb-0">
-                                {{ $cultivo_fase->nombre }}
-                                <!-- {{ $cultivo_fase->cultivo_fase_id }} -->
-                            </h4>
-                            <!-- {{$cultivo_fase->cultivo_id}} -->
-                            <form action="{{ route('destroyCultivoFase') }}" method="POST">
-                                @csrf
-                                @method('POST')
-                                <input type="hidden" name="cultivo_fase_id" value="{{ $cultivo_fase->cultivo_fase_id }}">
-                                <input type="hidden" name="cultivo_id" value="{{ $cultivo_fase->cultivo_id }}">
-                                <button class="inline-block px-4 py-2.5 bg-red-600 text-white font-medium text-xs
+                    @foreach ($cultivo_fases as $cultivo_fase)
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <div class="justify-between d-flex align-items-center">
+                                    <h4 class="h4 mb-0">
+                                        {{ $cultivo_fase->nombre }}
+                                        <!-- {{ $cultivo_fase->cultivo_fase_id }} -->
+                                    </h4>
+                                    <!-- {{ $cultivo_fase->cultivo_id }} -->
+                                    <form action="{{ route('destroyCultivoFase') }}" method="POST">
+                                        @csrf
+                                        @method('POST')
+                                        <input type="hidden" name="cultivo_fase_id"
+                                            value="{{ $cultivo_fase->cultivo_fase_id }}">
+                                        <input type="hidden" name="cultivo_id"
+                                            value="{{ $cultivo_fase->cultivo_id }}">
+                                        <button
+                                            class="inline-block px-4 py-2.5 bg-red-600 text-white font-medium text-xs
                             leading-tight uppercase rounded
                             shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700
                             focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg
-                            transition duration-150 ease-in-out " type="submit">
-                                    Eliminar
-                                </button>
+                            transition duration-150 ease-in-out "
+                                            type="submit">
+                                            Eliminar
+                                        </button>
 
-                            </form>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div>
-                            <div class="justify-end d-flex align-center">
-                                <livewire:agregar-insumo-modal :cultivo_fase_id="$cultivo_fase->cultivo_fase_id" />
-                                <livewire:agregar-actividad-modal :cultivo_fase_id="$cultivo_fase->cultivo_fase_id" />
-                                <livewire:agregar-costos-modal />
-                                <livewire:ver-cultivo-fase-modal :cultivo_fase_id="$cultivo_fase->cultivo_fase_id" />
-
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div>
+                                    <div class="justify-end d-flex align-center">
+                                        <livewire:ver-cultivo-fase-modal :cultivo_fase_id="$cultivo_fase->cultivo_fase_id" />
+                                        <livewire:agregar-insumo-modal :cultivo_fase_id="$cultivo_fase->cultivo_fase_id" />
+                                        <livewire:agregar-actividad-modal :cultivo_fase_id="$cultivo_fase->cultivo_fase_id" />
+                                        <livewire:agregar-costos-modal ::cultivo_fase_id="$cultivo_fase - > cultivo_fase_id" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                @endforeach
+                    @endforeach
 
-            </div>
+                </div>
             @endif
 
         </div>
